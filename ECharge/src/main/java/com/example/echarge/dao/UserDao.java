6f994376f1becc6c5ce41699c438212448585dae
KQ <1969676821@qq.com>
2022-05-20 00:36:26 +0800

    KQ test

--- ECharge/src/main/java/com/example/echarge/dao/UserDao.java
+++ ECharge/src/main/java/com/example/echarge/dao/UserDao.java
@@ -3,7 +3,9 @@ package com.example.echarge.dao;
 import com.example.echarge.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserDao extends JpaRepository<UserEntity, String> {
+    UserEntity findByUid(int uid);
     UserEntity findByWxId(String wxId);
     UserEntity findByToken(String sessionKey);
     UserEntity findByName(String nickName);
--- ECharge/src/main/java/com/example/echarge/service/UserService.java
+++ ECharge/src/main/java/com/example/echarge/service/UserService.java
@@ -1,4 +1,27 @@
 package com.example.echarge.service;
 
+import com.example.echarge.dao.UserDao;
+import com.example.echarge.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserDao userDao;
+
+    public UserEntity showUserInfo(int uid){
+        return userDao.findByUid(uid);
+    }
+
+    public LinkedHashMap<String, Object> assembleUserInfo(UserEntity user) {
+        LinkedHashMap<String, Object> res = new LinkedHashMap<String, Object>(0);
+        res.put("name", user.getName());
+        res.put("gender",user.getGender());
+        res.put("phone",user.getPhoneNumber());
+        res.put("iconUrl",user.getIconUrl());
+        return res;
+    }
 }
